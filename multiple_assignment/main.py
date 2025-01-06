28697637fa847dd8117358075cc50ed784b2e9bc
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-06 22:37:25 +0000

    Added multiple_assignment project

---
+++ multiple_assignment/main.py
@@ -0,0 +1,7 @@
+# Assign multiple variables in one line
+a, b, c = 1, 2, 3
+print("Variables assigned in one line:", a, b, c)  # 1 2 3
+
+# Assign multiple variables with the same value in one line
+a = b = c = 4
+print("Variables assigned with the same value in one line:", a, b, c)  # 4 4 4

92fea8a8c94f69b46357e908b4e6a7be0b9fd353
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-06 22:58:00 +0000

    Added string methods project

---
+++ string_methods/main.py
@@ -0,0 +1,16 @@
+# Define string
+msg = 'Hello'
+
+# Print string using different methods
+print("msg:", msg)
+print("msg.lower():", msg.lower())
+print("msg.upper():", msg.upper())
+print("msg.capitalise():", msg.capitalize())
+print("msg.title():", msg.title())
+print("msg.swapcase():", msg.swapcase())
+print("len(msg):", len(msg))
+print("msg.count('l'):", msg.count('l'))
+print("msg.find('l'):", msg.find('l'))
+print("msg.isdigit():", msg.isdigit())
+print("msg.isalpha():", msg.isalpha())
+print("msg*3:", msg*3)

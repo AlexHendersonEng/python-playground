2b5e9d1095dc7c59d9e4ebd180ab5c70aac7da40
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-07 19:04:23 +0000

    Added user_input project

---
+++ user_input/main.py
@@ -0,0 +1,9 @@
+# Get input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+# Print to standard output
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height}")

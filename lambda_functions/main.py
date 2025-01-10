ac35ab9504ec39f0e8ab53b199268036aa7e2c63
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-10 22:06:05 +0000

    Added lambda_functions project

---
+++ lambda_functions/main.py
@@ -0,0 +1,8 @@
+# lambda function: A function written in one line using the lambda keyword.abs
+#                  lambda <parameters> : <expressioon>
+
+# Define lambda function
+multiply = lambda x, y : x * y
+
+# Call lambda function
+print(multiply(3, 4))

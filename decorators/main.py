ce22bf74627849487d8f630886a7c6b14622aa31
AlexHendersonEng <Alexhendersnao@gmail.com>
2025-01-11 12:22:53 +0000

    Updated decorators project

--- decorators/main.py
+++ decorators/main.py
@@ -1,8 +1,7 @@
 # Decorator: A function that takes another function as an argument and returns
-#            a new function with enhanced functionality. Decorators often use the
-#            shorthand '@' syntax as a shorthand for 'func = decorator(func)'.
+#            a new function with enhanced functionality.
 
-# Define decorator function
+# Define basic decorator function
 def decorator(func):
 
     def wrapper():
@@ -12,10 +11,38 @@ def decorator(func):
 
     return wrapper
 
-# Apply decorator to function
-@decorator
+# Apply basic decorator to function
+@decorator # Equivalent to 'hello = decorator(hello)'
 def hello():
     print("Hello World!")
 
-# Call hello function
+# Call function
+print("Basic decorator:")
 hello()
+print()
+
+# Define decorator function where decorator and function take arguments
+def decorator1(message):
+
+    def inner(func):
+
+        def wrapper(*args, **kwargs):
+            print(message)
+            print("Pre-function call")
+            func(*args, **kwargs)
+            print("Post-function call")
+
+        return wrapper
+
+    return inner
+
+# Apply decorator to function
+@decorator1("Decorator function") # Equivalent to 'print_list = (decorator1(message))(print_list)'
+def print_list(items):
+    for item in items:
+        print(item)
+
+# Call function
+print("Decorator function where decorator and decorated function take arguments:")
+print_list(["One", "Two", "Three"])
+print()
